385833e8801a169eb7abdb9791fa688c8deab18f
Ivan Bilotserkivskiy <ivanbilotserkivskiy56@gmail.com>
2023-10-16 17:52:50 +0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,11 @@ const PORT = process.env.PORT || 3001;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    methods: ['GET', 'POST'],
+    credentials: true,
+  });
   await app.listen(PORT);
   console.log(`Server runing on: http://localhost${PORT}`);
 }
